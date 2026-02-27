987c1e2aa9ad32631deb03d8d1443731f2d2e7f1
FoXeNe <dimon20306@mail.ru>
2026-02-27 18:48:23 +0300

    refactor(main): move InputManager logic

--- app/src/main/kotlin/lab5/Main.kt
+++ app/src/main/kotlin/lab5/Main.kt
@@ -1,48 +1,9 @@
 package lab5
 
-import lab5.manager.CollectionManager
-import lab5.model.Coordinates
-import lab5.model.Organization
-import lab5.model.Product
-import java.time.ZonedDateTime
-import java.util.LinkedList
-import kotlin.text.toLong
-import kotlin.text.toLongOrNull
+import lab5.command.InputManager
 
 fun main() {
-    val manager = CollectionManager()
+    val input = InputManager()
 
-    println("введите имя")
-    val name = readLine() ?: "default"
-
-    var price: Long?
-
-    do {
-        println("введите цену:")
-        var input = readLine()
-
-        price = input?.toLongOrNull()
-
-        if (price == null) {
-            println("цена должна быть числом")
-        } else if (price <= 0) {
-            println("цена должна быть больше 0")
-            price = null
-        }
-    }
-    while (price == null)
-
-    val newProduct =
-        Product(
-            id = 1,
-            name = name,
-            // TODO: do this coordinates section
-            coordinates = Coordinates(10L, 10F),
-            creationDate = ZonedDateTime.now(),
-            price = price,
-            unitOfMeasure = null,
-            // TODO: do this Organization section
-            manufacturer = Organization(1L, "test", "testtest", 10L),
-        )
-    manager.addProduct(newProduct)
+    input.productInput()
 }
