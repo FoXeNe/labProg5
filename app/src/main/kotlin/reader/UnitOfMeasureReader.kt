d236615512421b35793e1bb3076d20c2aeb38ff3
FoXeNe <dimon20306@mail.ru>
2026-03-03 14:12:40 +0300

    feat(command): add validation to UnitOfMeasureReader

--- app/src/main/kotlin/reader/UnitOfMeasureReader.kt
+++ app/src/main/kotlin/reader/UnitOfMeasureReader.kt
@@ -7,12 +7,18 @@ class UnitOfMeasureReader(
     private val io: IOHandler,
 ) {
     fun read(): UnitOfMeasure? {
-        io.println("выберите нужную единицу измерения или ничего не пишите")
-        for (values in UnitOfMeasure.values()) {
-            io.println(values.toString())
+        while (true) {
+            io.println("выберите нужную единицу измерения или ничего не пишите")
+            for (values in UnitOfMeasure.values()) {
+                io.println(values.toString())
+            }
+            val input = io.readLine()
+            if (input.isNullOrBlank()) return null
+            try {
+                return UnitOfMeasure.valueOf(input.uppercase())
+            } catch (e: IllegalArgumentException) {
+                io.println("ввод не является UnitOfMeasure, попробуйте еще раз")
+            }
         }
-        val input = io.readLine()
-        if (input.isNullOrBlank()) return null
-        return UnitOfMeasure.valueOf(input.uppercase())
     }
 }
