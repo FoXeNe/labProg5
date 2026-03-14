e4e083d0730de9424af60d98cf50cefdfca2fd31
FoXeNe <dimon20306@mail.ru>
2026-03-15 01:12:50 +0300

    feat(io): add io wrapper

--- app/src/main/kotlin/app/AppInitializer.kt
+++ app/src/main/kotlin/app/AppInitializer.kt
@@ -1,36 +1,24 @@
 package app
 
-import command.commands.Add
-import command.commands.Clear
-import command.commands.Exit
-import command.commands.Help
-import command.commands.History
-import command.commands.Info
-import command.commands.RemoveById
-import command.commands.RemoveFirst
-import command.commands.Show
-import command.commands.Update
-import io.IOHandler
+import command.commands.*
+import io.IOWrapper
 import manager.CollectionManager
 import manager.CommandManager
 
 class AppInitializer {
     fun setup(
         commandManager: CommandManager,
-        io: IOHandler,
-        app: AppExecutor,
+        io: IOWrapper,
     ) {
         val collectionManager = CollectionManager(io)
 
         commandManager.register(Add(io, collectionManager))
-        commandManager.register(Exit(io) { app.stop() })
         commandManager.register(Clear(io, collectionManager))
         commandManager.register(Help(io, commandManager))
-        commandManager.register(Show(io, collectionManager))
+        commandManager.register(History(io, commandManager))
         commandManager.register(Info(io, collectionManager))
-        commandManager.register(Update(io, collectionManager))
         commandManager.register(RemoveById(io, collectionManager))
         commandManager.register(RemoveFirst(io, collectionManager))
-        commandManager.register(History(io, commandManager))
+        commandManager.register(Update(io, collectionManager))
     }
 }
