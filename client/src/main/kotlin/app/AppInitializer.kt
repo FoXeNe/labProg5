e3915413a8e35c17013011465cd8ab48ff53d916
FoXeN <dimon20306@mail.ru>
2026-04-23 16:29:45 +0300

    feat(client): add all commands register to AppInitializer

--- client/src/main/kotlin/app/AppInitializer.kt
+++ client/src/main/kotlin/app/AppInitializer.kt
@@ -3,28 +3,30 @@ package app
 import command.commands.*
 import io.IOWrapper
 import manager.CommandManager
-import java.io.File
-import java.util.LinkedList
-
-const val ENV_FILE = "COLLECTION_FILE"
+import manager.NetworkManager
 
 class AppInitializer {
     fun setup(
         commandManager: CommandManager,
         io: IOWrapper,
         app: AppExecutor,
+        network: NetworkManager,
     ) {
         commandManager.register(Help(io, commandManager))
         commandManager.register(History(io, commandManager))
         commandManager.register(Exit(io, { app.stop() }))
         commandManager.register(ExecuteScript(io, commandManager))
-    }
 
-    private fun createWalPath(mainFilePath: String?): String =
-        if (mainFilePath != null) {
-            "$mainFilePath.wal"
-        } else {
-            val tmpDir = System.getProperty("java.io.tmpdir")
-            File(tmpDir, "collection_session.wal").absolutePath
-        }
+        commandManager.register(Add(io, network))
+        commandManager.register(AddIfMin(io, network))
+        commandManager.register(Clear(io, network))
+        commandManager.register(FilterByManufacturer(io, network))
+        commandManager.register(FilterGreaterThanManufacturer(io, network))
+        commandManager.register(Info(io, network))
+        commandManager.register(RemoveById(io, network))
+        commandManager.register(RemoveFirst(io, network))
+        commandManager.register(Show(io, network))
+        commandManager.register(SumOfPrice(io, network))
+        commandManager.register(Update(io, network))
+    }
 }
